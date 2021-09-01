fbcb1f8771b4229753239d7213f16d8be7ea9022
ruhipandey77 <89899962+ruhipandey77@users.noreply.github.com>
2021-09-01 17:21:12 +0530

    implemented POM
    implemented POM

--- src/test/java/LuminorTest.java
+++ src/test/java/LuminorTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,9 +14,11 @@ import static com.codeborne.selenide.Selenide.screenshot;
 public class LuminorTest {
     By currencyDropdown = By.xpath("(//div[@class=\"currency-select-inner\"])[1]");
     By convertedText = By.xpath("(//input)[2]");
-    @Test
-    public void testLuminor() throws InterruptedException, IOException {
+    By inputText = By.cssSelector("input");
 
+    @BeforeTest
+    public String[] returnAmountCurrencyPath() throws IOException {
+        String[] returnValues = new String[2];
 
         FileInputStream fis = new FileInputStream("LuminorTest.xlsx");
         XSSFWorkbook workbook = new XSSFWorkbook(fis);
@@ -27,14 +30,24 @@ public class LuminorTest {
         String currency = sheet.getRow(1).getCell(0).getStringCellValue();
         String currencyPath = "(//button[text()='" + currency + "'])[1]";
 
+        returnValues[0] = amount;
+        returnValues[1] = currencyPath;
+
+        return returnValues;
+    }
+
+    @Test
+    public void testLuminor() throws IOException {
+
+        String[] returnValues = returnAmountCurrencyPath();
+
         open("https://luminor.ee/currency-rates");
-        $("input").waitUntil(visible, 20000);
+        $(inputText).waitUntil(visible, 20000);
 
         $(currencyDropdown).click();
-        $(By.xpath(currencyPath)).click();
+        $(By.xpath(returnValues[1])).click();
 
-
-        $("input").setValue(amount).pressEnter();
+        $(inputText).setValue(returnValues[0]).pressEnter();
         String convertedAmount= $(convertedText).getValue();
 
         screenshot("my_file_name");
